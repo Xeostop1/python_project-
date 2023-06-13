66f29cd4355503d780d8012d7225ae06f50305e8
hanaseo <11reqeuiem27@gmail.com>
2023-06-13 10:59:55 +0900

    home check

--- final_ex/agoda_extract.py
+++ final_ex/agoda_extract.py
@@ -20,6 +20,7 @@ options.add_argument('"detach", True')
 options.add_argument("window-size=1920x1080")
 options.add_argument(f"user-agent={user_agent}")
 browser=webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
+browser.maximize_window()
 
 loading = random.randint(5, 12)
 wait = WebDriverWait(browser, 10)
@@ -41,9 +42,7 @@ pre_input.click()
 print("pre_input 클릭")
 time.sleep(loading)
 
-keyword="시드니"
-pre_input.send_keys(keyword)
-print("키워드 입력")
+
 
 # pre_click=browser.find_element(By.CSS_SELECTOR, 'ul[role="listbox"]')
 # pre_click.click()
@@ -54,33 +53,83 @@ time.sleep(loading)
 print("input 찾음")
 input.click()
 
+keyword="시드니"
+pre_input.send_keys(keyword)
+print("키워드 입력")
+
+
 date_piker=browser.find_element(By.ID,"check-in-box").click()
 print("date_piker 클릭")
 time.sleep(loading)
 
-in_day=browser.find_element(By.CSS_SELECTOR, 'span[data-selenium-date="2023-06-12"]')
+in_day=browser.find_element(By.CSS_SELECTOR, 'span[data-selenium-date="2023-06-16"]')
 print("date_piker 서치")
 in_day.click()
 print("in_day 클릭")
 
 time.sleep(loading)
-out_day=browser.find_element(By.CSS_SELECTOR, 'span[data-selenium-date="2023-07-19"]')
+out_day=browser.find_element(By.CSS_SELECTOR, 'span[data-selenium-date="2023-07-02"]')
 print("date_piker 서치")
 out_day.click()
 print("out_day 클릭")
 time.sleep(loading)
 
-search_btn=browser.find_element(By.CLASS_NAME, "Searchbox__searchButton__text")
+search_btn=browser.find_element(By.CSS_SELECTOR, 'button[data-element-name="search-button"]')
 search_btn.click()
-
+time.sleep(loading)
 
 
 #=====데이터 추출====== 
-
+last_page=2
+page_num=1
 while True:
-    page_num=1
     print("현재페이지", page_num)
     soup=BeautifulSoup(browser.page_source, "html.parser")
-    item_list=soup.select('ol.hotel-list-container')
+    # item_list=soup.select('ol.hotel-list-container')
+    item_list=soup.select('.PropertyCard__Link')
+    option_list=[]
     for i in item_list:
-        print(i)
+        name=i.select_one('h3[data-selenium="hotel-name"]')
+        if name:
+            name=name.get_text().strip()
+        else:
+            name="이름없음"
+        print("호텔명", name)
+        
+        area= i.select_one('span.c-kEjbxe .sc-iqHYGH .bpoMrU .hGjjJo .sc-dQppl .cQZIoF')
+        if area:
+            area=area.get_text().strip()
+        else:
+            area="지역없음"
+        print("지역", area)
+        
+        price=i.select_one('span[data-selenium="display-price"]')
+        if price:
+            price=price.get_text().strip()
+        else:
+            price="가격없음"    
+        print("가격", price,"\n")
+        options=i.select('div[data-selenium="pill-container"]')
+        if options:
+            for option in options:
+                option_item=option.select_one('div[data-element-name="pill-each-item"]').get_text().strip()
+                option_list.append(option_item)
+        else:
+            option="옵션없음"
+        
+        
+        # for i in option_list:
+        #     print(i)
+    page_num+=1
+    if page_num==last_page:
+        break
+    try:
+       next_page_ele=browser.find_element(By.ID, 'paginationNext')
+       print(f"찾음 다음페이지 {page_num+1}")
+       time.sleep(loading)
+       next_page_ele.click()
+       time.sleep(loading)
+       print(f"클릭 다음페이지 {page_num+1}")
+       #클릭 한후에는 와일문 처음으로 돌아 가고 위에 if 브레이크 만나기 전까지 계속 실행 
+    except:
+        print(f"에러: 버튼 미추출 또는 미클릭 ******{page_num+1}")
